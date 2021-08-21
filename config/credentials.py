4ccab8525ed9a2c1fc48e15672a5bd9378db3e9d
Richard Mwewa <74001397+rlyonheart@users.noreply.github.com>
2021-08-21 14:48:16 +0200

    Create credentials.py

---
+++ config/credentials.py
@@ -0,0 +1,8 @@
+# authenticated user credentials
+# client_id and client_secret can be found at https://old.reddit.com/prefs/apps, you will need to have an account on Reddits
+
+client_id = "client_id"
+client_secret = "client_secret"
+username = "username"
+password = "password"
+user_agent = "User-Agent"
